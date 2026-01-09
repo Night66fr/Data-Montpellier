bf47c665016f81b3da11ae1c5072e4516f533459
Ghost41500 <nath.place415@gmail.com>
2026-01-09 10:51:31 +0100

    Changement V1

--- Data-processing/Procesing.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-def convertir_en_variables(dictionnaire):
-    for cle, valeurs in dictionnaire.items():
-        nom_variable = cle.replace(" ", "_").replace("-", "_")
-        print(f"{nom_variable} = {valeurs}")
-
-print(convertir_en_variables(a))
